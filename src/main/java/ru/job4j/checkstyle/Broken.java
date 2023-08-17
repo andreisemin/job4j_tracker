36b36fa845881b68e7bf65a4b9f4cda553d1b4a7
andreisemin <andr-syomin@yandex.ru>
2023-08-17 15:17:57 +0400

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,28 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    private int sizeOfEmpty = 10;
+
+    public String surname;
+
+    public static final String NEW_VALUE = "";
+
+    public void echo() {
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+
+    public String name;
+
+    public Broken() {
+    }
+}
+
