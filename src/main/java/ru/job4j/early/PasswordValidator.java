be4ea3806946748d041b7f876b6a34bf08be08ed
andreisemin <andr-syomin@yandex.ru>
2023-09-18 10:19:45 +0400

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -10,72 +10,52 @@ public class PasswordValidator {
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-        if (!containsUpperCase(password)) {
-            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
-        }
-
-        if (!containsLowerCase(password)) {
-            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
-        }
-
-        if (!containsDigit(password)) {
-            throw new IllegalArgumentException("Password should contain at least one figure");
-        }
-
-        if (!containsSpecialSymbol(password)) {
-            throw new IllegalArgumentException("Password should contain at least one special symbol");
-        }
-
-        if (containsForbiddenSubstrings(password)) {
-            throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
-        }
-
-        return password;
-    }
-
-    private static boolean containsUpperCase(String password) {
+        boolean hasUpCase = false;
+        boolean hasLowCase = false;
+        boolean hasDigit = false;
+        boolean hasSpecial = false;
         for (char symbol : password.toCharArray()) {
             if (Character.isUpperCase(symbol)) {
-                return true;
+                hasUpCase = true;
             }
-        }
-        return false;
-    }
-
-    private static boolean containsLowerCase(String password) {
-        for (char symbol : password.toCharArray()) {
             if (Character.isLowerCase(symbol)) {
-                return true;
+                hasLowCase = true;
             }
-        }
-        return false;
-    }
-
-    private static boolean containsDigit(String password) {
-        for (char symbol : password.toCharArray()) {
             if (Character.isDigit(symbol)) {
-                return true;
+                hasDigit = true;
             }
-        }
-        return false;
-    }
-
-    private static boolean containsSpecialSymbol(String password) {
-        for (char symbol : password.toCharArray()) {
             if (!Character.isLetterOrDigit(symbol)) {
-                return true;
+                hasSpecial = true;
             }
         }
-        return false;
-    }
-
-    private static boolean containsForbiddenSubstrings(String password) {
+        if (!hasUpCase) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one uppercase letter"
+            );
+        }
+        if (!hasLowCase) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one lowercase letter"
+            );
+        }
+        if (!hasDigit) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one figure"
+            );
+        }
+        if (!hasSpecial) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one special symbol"
+            );
+        }
         String passwordLowerCase = password.toLowerCase();
         for (String forbidden : FORBIDDEN) {
             if (passwordLowerCase.contains(forbidden)) {
-                return true;
+                throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
             }
         }
-        return false;
+        return password;
     }
 }
+
+
