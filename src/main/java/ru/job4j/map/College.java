48a9e63fac35432637eabfa0f5be155db2d092c0
andreisemin <andr-syomin@yandex.ru>
2023-12-13 11:55:01 +0400

    3. Optional - empty, of [#214711]

---
+++ src/main/java/ru/job4j/map/College.java
@@ -0,0 +1,29 @@
+package ru.job4j.map;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class College {
+
+    private final Map<Student, Set<Subject>> students;
+
+    public College(Map<Student, Set<Subject>> students) {
+        this.students = students;
+    }
+
+    public Optional<Student> findByAccount(String account) {
+        return students.keySet()
+                .stream()
+                .filter(s -> s.account().equals(account))
+                .findFirst();
+    }
+
+    public Optional<Subject> findBySubjectName(String account, String name) {
+        Optional<Student> student = findByAccount(account);
+        return student.flatMap(s -> students.get(s)
+                .stream()
+                .filter(sub -> sub.name().equals(name))
+                .findFirst());
+    }
+}
